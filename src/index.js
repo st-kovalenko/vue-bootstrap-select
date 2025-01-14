ec99b490172603e4760f3d7ee41ac361a0b41f64
Stanislav Ernst <stas@ln-s.de>
2025-01-14 14:27:06 +0100

    FIX - Simplify export of VueBootstrapSelect component.
    Replaced the plugin-style export with a direct default export statement. This change reduces redundancy and aligns with a more straightforward module export approach.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-import VueBootstrapSelect from "@/components/VueBootstrapSelect.vue";
-
-export default {
-    install: (app, options) => {
-        app.component("VueBootstrapSelect", VueBootstrapSelect);
-    },
-};
+export { default } from "@/components/VueBootstrapSelect.vue";
